b5a49fe463c80d5f0e5e3783d08cf747c105cd18
miguel260383 <miguelrodriguez2603@gmail.com>
2025-11-23 19:41:43 +0000

    Miguel260383

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-import "bootstrap";
-import "./style.css";
+const choises = ["rock", "paper", "scissors", "lizard", "spock"]
+const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors:["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"] 
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const jugar = (userChoise) => {
+console.log(Math.floor(Math.random()*choises.length));
+console.log(choises[Math.floor(Math.random()*choises.length)]);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const computerChoise = choises[Math.floor(Math.random()*choises.length)]
+console.log("userChoise: ", userChoise);
+console.log("computerChoise: ", computerChoise);
+
+if (userChoise === computerChoise) {
+    console.log("Empate");
+}
+
+else if (userChoise === "lizard" && (computerChoise === "spock" || computerChoise === "paper")){
+    console.log("Ganaste")
+}
+else if (userChoise === "rock" && (computerChoise === "scissors" || computerChoise === "lizard")){
+    console.log("Ganaste")
+}
+else if (userChoise === "paper" && (computerChoise === "spock" || computerChoise === "rock")){
+    console.log("Ganaste")
+}
+else if (userChoise === "scissors" && (computerChoise === "paper" || computerChoise === "lizard")){
+    console.log("Ganaste")
+}
+else if (userChoise === "spock" && (computerChoise === "scissors" || computerChoise === "rock")){
+    console.log("Ganaste")
+}
+else {
+    console.log("Perdiste")
+}
+
+}
+jugar("spock")
